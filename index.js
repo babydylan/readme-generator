2501f8a98ecf0270e69b524a10d15eb78cedee6c
Dylan Nguyen <babydylannguyen@gmail.com>
2020-10-01 18:45:53 -0700

    add questions to question array

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+
+
 // array of questions for user
 const questions = [
-
+    "What is the app's title?",
+    "Enter Description of app.",
+    "What are the steps required to install your app?",
+    "What are the instructions to use the app?",
+    "List any collaborators you had with links to their GitHub profiles and/or any third-party assets that require attribution, and/or tutorial links you followed.",
+    "List any tests you have for the app and how to run them.",
+    "What is your GitHub username>",
+    "What is your email address?",
+    
 ];
 
 // function to write README file
