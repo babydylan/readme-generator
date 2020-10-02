acc1748868c3658d8b2a468413fad6da5664c696
Dylan Nguyen <babydylannguyen@gmail.com>
2020-10-01 19:18:15 -0700

    add inquirer prompts

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 ## Description 
 
-## Table of Contents (Optional)
-
-If your README is very long, add a table of contents to make it easy for users to find what they need.
+## Table of Contents
 
 * [Installation](#installation)
 * [Usage](#usage)
@@ -15,30 +13,13 @@ If your README is very long, add a table of contents to make it easy for users t
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
 
 ## Usage 
 
-Provide instructions and examples for use. Include screenshots as needed. 
-
-
 ## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
 
 ## Contributing
 
-If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
-
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them.
 
-
----
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- index.js
+++ index.js
@@ -5,18 +5,60 @@ const util = require("util");
 
 
 // array of questions for user
-const questions = [
-    "What is the app's title?",
-    "Enter Description of app.",
-    "What are the steps required to install your app?",
-    "What are the instructions to use the app?",
-    "List any collaborators you had with links to their GitHub profiles and/or any third-party assets that require attribution, and/or tutorial links you followed.",
-    "List any tests you have for the app and how to run them.",
-    "What is your GitHub username>",
-    "What is your email address?",
-    
-];
-
+// const questions = [
+//     "What is the app's title?",
+//     "Enter Description of app.",
+//     "What are the steps required to install your app?",
+//     "What are the instructions to use the app?",
+//     "List any collaborators you had with links to their GitHub profiles and/or any third-party assets that require attribution, and/or tutorial links you followed.",
+//     "List any tests you have for the app and how to run them.",
+//     "What is your GitHub username?",
+//     "What is your email address?"
+// ];
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is your name?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Enter Description of app.",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What are the steps required to install your app?",
+        },
+        {
+            type: "input",
+            name: "Usage",
+            message: "What are the instructions to use the app?",
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "List any collaborators you had with links to their GitHub profiles and/or any third-party assets that require attribution, and/or tutorial links you followed.",
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "List any tests you have for the app and how to run them.",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?",
+        },
+    ])
+}
 // function to write README file
 function writeToFile(fileName, data) {
 }
